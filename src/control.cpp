8bf66229dd2883348f58da51d02fc39df3e57f3f
daniediv <das.29@outlook.com>
2022-06-29 12:28:34 -0300

    remocaod de ingrediente em cascata

--- src/control.cpp
+++ src/control.cpp
@@ -49,13 +49,76 @@ void Control::removerReceitaEmCascata() {
 
     // assumindo que o id exista
 
-    string queryIngredientes = "select * from receitas_ingredientes where receita_id = ";
+    string queryIngredientes = "select receita_id, ingrediente_id from receitas_ingredientes where receita_id = ";
     queryIngredientes.append(id);
 
-    getUtil()->imprimeDados(getMysql(), queryIngredientes.c_str());
+    // getUtil()->imprimeDados(getMysql(), queryIngredientes.c_str());
 
     // buscarIngredientesDaReceita(rec);
     // buscarEtapasDaReceita(rec);
+
+    mysql_query(getMysql(), queryIngredientes.c_str());
+    MYSQL_RES *resultado = mysql_store_result(mysql);
+    MYSQL_ROW linha;
+
+    if (resultado == NULL) {
+        cout << "Query invalida" << endl;
+    } else if (mysql_affected_rows(mysql) == 0) {
+        cout << "Nao possui ingredientes nesta receita" << endl;
+    }
+
+    while ((linha = mysql_fetch_row(resultado))) {
+        query.assign("delete from receitas_ingredientes where receita_id = ")
+            .append(id).append(" and ingrediente_id = ").append(linha[1]);
+        // cout << "query: " << query << endl;
+        removerLinha(query);
+    }
+
+    string queryEtapas = "select receita_id, numero from receitas_etapas where receita_id = ";
+    queryEtapas.append(id);
+
+    // getUtil()->imprimeDados(getMysql(), queryEtapas.c_str());
+
+    mysql_query(getMysql(), queryEtapas.c_str());
+    resultado = mysql_store_result(mysql);
+
+    if (resultado == NULL) {
+        cout << "Query invalida" << endl;
+    } else if (mysql_affected_rows(mysql) == 0) {
+        cout << "Nao possui etapas nesta receita" << endl;
+    }
+
+    while ((linha = mysql_fetch_row(resultado))) {
+        // query.assign("delete from receitas_ingredientes where receita_id = ")
+        //     .append(id).append(" and numero = ").append(linha[1]);
+        // cout << "query: " << query << endl;
+        // removerLinha(query);
+
+        string queryPassosDaEtapa = "select receita_id, etapa_numero, sequencia from receitas_passos where receita_id = ";
+        queryPassosDaEtapa.append(id).append(" and etapa_numero = ").append(linha[1]);
+
+        // getUtil()->imprimeDados(getMysql(), queryEtapas.c_str());
+
+        mysql_query(getMysql(), queryPassosDaEtapa.c_str());
+        MYSQL_RES *resultado2 = mysql_store_result(mysql);
+        MYSQL_ROW linha2;
+
+        // cout << "query etapas: " << queryEtapas << endl;
+        // cout << "query passos:" << queryPassosDaEtapa << endl;
+
+        while ((linha2 = mysql_fetch_row(resultado2))) {
+            queryPassosDaEtapa.assign("delete from receitas_passos where receita_id = ")
+                .append(id).append(" and etapa_numero = ").append(linha2[1])
+                .append(" and sequencia = ").append(linha2[2]);
+            removerLinha(queryPassosDaEtapa);
+        }
+        queryEtapas.assign("delete from receitas_etapas where receita_id = ")
+            .append(id).append(" and numero = ").append(linha[1]);
+        // cout << "query: " << queryEtapas << endl;
+        removerLinha(queryEtapas);
+    }
+    query.assign("delete from receitas where id = ").append(id);
+    removerLinha(query);
 }
 
 void Control::adicionarIngrediente() {
